39fcffda44b91c958c5d1333c89cdb22da0737b3
Hemanth Chand <hemangudivada@gmail.com>
2026-03-18 16:04:33 +0530

    remove item from wishlist testcase addedd

--- pages/wishlist_page.py
+++ pages/wishlist_page.py
@@ -7,9 +7,15 @@ class WishlistPage(BasePage):
     wish_list_link = "//a[@id='wishlist-total']"
     home_icon = "//a[text()='Qafox.com']"
     mac_book_link_in_wishlist_table = "//div[@id='content']//table[contains(@class,'table')]/tbody//a[normalize-space()='MacBook']"
+    remove_icon_wishlist_page  = "(//button[@data-original-title='Add to Cart']/following::a)[1]"
+    wish_list_empty_text = "//p[text()='Your wish list is empty.']"
 
     def add_item_to_wishlist(self):
         self.click(self.home_icon)
         self.click(self.mac_book_item)
         self.click(self.wish_list_button)
         self.click(self.wish_list_link)
+
+    def remove_item_from_wishlist(self):
+        self.click(self.wish_list_link)
+        self.click(self.remove_icon_wishlist_page)
--- tests/wish_list_test.py
+++ tests/wish_list_test.py
@@ -2,8 +2,13 @@ from playwright.sync_api import expect
 
 from pages.wishlist_page import WishlistPage
 
-
 def test_add_item_to_wish_list(page,logger,log_in_to_page):
     wish_list = WishlistPage(page,logger)
     wish_list.add_item_to_wishlist()
     expect(page.locator(wish_list.mac_book_link_in_wishlist_table)).to_be_visible()
+
+def test_remove_item_from_wishlist(page,logger,log_in_to_page):
+    wish_list = WishlistPage(page,logger)
+    wish_list.add_item_to_wishlist()
+    wish_list.remove_item_from_wishlist()
+    expect(page.locator(wish_list.wish_list_empty_text)).to_be_visible()
